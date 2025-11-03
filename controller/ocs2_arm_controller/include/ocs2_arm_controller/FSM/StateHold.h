017e69ad179495e70380c1bdb4da020fdcc0c2b1
Huang Zhenbiao <biao876990970@hotmail.com>
2025-11-03 15:45:02 +0800

    state hold refactor

--- controller/ocs2_arm_controller/include/ocs2_arm_controller/FSM/StateHold.h
+++ controller/ocs2_arm_controller/include/ocs2_arm_controller/FSM/StateHold.h
@@ -1,14 +1,10 @@
 //
 // Created for OCS2 Arm Controller - StateHold
 //
-
-#ifndef STATEHOLD_H
-#define STATEHOLD_H
+#pragma once
 
 #include <vector>
 #include <memory>
-
-#include <rclcpp/rclcpp.hpp>
 
 #include "ocs2_arm_controller/Ocs2ArmController.h"
 
@@ -17,7 +13,8 @@ namespace ocs2::mobile_manipulator
     class StateHold : public FSMState
     {
     public:
-        StateHold(CtrlInterfaces& ctrl_interfaces, const std::shared_ptr<CtrlComponent>& ctrl_comp = nullptr);
+        StateHold(CtrlInterfaces& ctrl_interfaces,
+                  const std::shared_ptr<CtrlComponent>& ctrl_comp = nullptr);
 
         void enter() override;
         void run(const rclcpp::Time& time, const rclcpp::Duration& period) override;
@@ -27,10 +24,11 @@ namespace ocs2::mobile_manipulator
     private:
         CtrlInterfaces& ctrl_interfaces_;
         std::shared_ptr<CtrlComponent> ctrl_comp_;  // CtrlComponent reference for torque calculation
+        std::shared_ptr<rclcpp_lifecycle::LifecycleNode> node_;  // Node reference for logging and parameters
         // State variables
         std::vector<double> hold_positions_; // Positions recorded when entering
         bool positions_recorded_{false};
+        // Threshold for joint position difference check (in radians)
+        double joint_position_threshold_; // Configurable threshold, default 0.1 rad (~5.7 degrees)
     };
 } // namespace ocs2_arm_controller
-
-#endif // STATEHOLD_H
